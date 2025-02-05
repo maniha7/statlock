a0fa160df67616b41b77b699806af160f13c5512
diegul <diegolvoice@gmail.com>
2025-02-04 20:25:19 -0500

    started support page

--- client/src/pages/Support.jsx
+++ client/src/pages/Support.jsx
@@ -1,12 +1,24 @@
-
+import globals from '../globals';
+import logo from '../assets/statlock_logo2.png'
+import { Link } from "react-router-dom";
+const gColors = globals.globalColors
 
 
 
 const Support = () => {
     return(
         <>
-        
-        
+        <div className="flex justify-center">
+            <div className={`mt-2 mb-2 ml-5 mr-10 p-2 border-b-4 border-l-1 border-r-2 border-stone-600 rounded-lg min-w-200 min-h-200  ${gColors.stoneBackgroundGradient}`}>
+                <div className="text-center">
+                test
+                </div>
+
+                <div>
+                    teoad
+                </div>
+            </div>
+        </div>
         </>
     )
 }
